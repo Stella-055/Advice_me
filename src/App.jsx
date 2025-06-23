99e700adb58b7140999cfa54939fc45449a443df
Stella-055 <stellaongayo243@gmail.com>
2025-06-23 18:11:43 +0300

    used prettier'

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,65 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { RiseLoader } from "react-spinners";
-import './App.css'
-
+import "./App.css";
 
 function App() {
-  const [advice, setAdvice] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-  
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-     const fetchAdvice =async () => {
-      
+    const fetchAdvice = async () => {
       try {
-        const response = await fetch('https://api.adviceslip.com/advic')
-        const data = await response.json()
-        setAdvice(data.slip.advice)
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        setAdvice(data.slip.advice);
       } catch (error) {
-        setAdvice('')
-       
-       setError('Error fetching advice')
-      
-      } 
-      
-    }; fetchAdvice();}, [])
+        setAdvice("");
+
+        setError("Error fetching advice");
+      }
+    };
+    fetchAdvice();
+  }, []);
 
   return (
     <>
-     <div>
-      <button onClick={
-       async ()=>{
-        setAdvice('')
-        setError(null)
-       setLoading(true)
-        try {
-          const response = await fetch('https://api.adviceslip.com/advice')
-          const data = await response.json()
-          setAdvice(data.slip.advice)
-        } catch (error) {
-          setAdvice('')
-          setError("Error fetching advice")
-          console.error('Error fetching advice:', error)
-          
-        }
-        finally {
-          setLoading(false)
-        }
-      }}
-      >Get random advice</button>
-      {loading &&  <RiseLoader
-        
-        loading={loading}
-        color="black"
-        size={20}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /> }
-  {error && <p>{error}</p>}
-     
-  
-      <p>{advice}</p>
-     </div>
+      <div>
+        <button
+          onClick={async () => {
+            setAdvice("");
+            setError(null);
+            setLoading(true);
+            try {
+              const response = await fetch("https://api.adviceslip.com/advice");
+              const data = await response.json();
+              setAdvice(data.slip.advice);
+            } catch (error) {
+              setAdvice("");
+              setError("Error fetching advice");
+              console.error("Error fetching advice:", error);
+            } finally {
+              setLoading(false);
+            }
+          }}
+        >
+          Get random advice
+        </button>
+        {loading && (
+          <RiseLoader
+            loading={loading}
+            color="black"
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        )}
+        {error && <p>{error}</p>}
+
+        <p>{advice}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

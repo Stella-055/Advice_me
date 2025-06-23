87f251083f424720059a2e08f58ef335584d4a36
Stella-055 <stellaongayo243@gmail.com>
2025-06-23 16:28:57 +0300

    functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
-import { useState } from 'react'
-
+import { useState,useEffect } from 'react'
+import { RiseLoader } from "react-spinners";
 import './App.css'
 
+
 function App() {
- 
+  const [advice, setAdvice] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  
+  useEffect(() => {
+    async function fetchAdvice ()  {
+      
+      try {
+        const response = await fetch('https://api.adviceslip.com/advice')
+        const data = await response.json()
+        setAdvice(data.slip.advice)
+      } catch (error) {
+        console.error('Error fetching advice:', error)
+      } 
+      
+    } fetchAdvice()}, [])
 
   return (
     <>
      <div>
       
-      <button>Get random advice</button>
+      <button onClick={
+       async ()=>{
+        setAdvice('')
+       setLoading(true)
+        try {
+          const response = await fetch('https://api.adviceslip.com/advice')
+          const data = await response.json()
+          setAdvice(data.slip.advice)
+        } catch (error) {
+          console.error('Error fetching advice:', error)
+          
+        }
+        finally {
+          setLoading(false)
+        }
+      }}
+      >Get random advice</button>
+      {loading &&  <RiseLoader
+        
+        loading={loading}
+        color="black"
+        size={20}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /> }
+  
+  
+      <p>{advice}</p>
      </div>
     </>
   )

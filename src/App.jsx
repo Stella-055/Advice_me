38a25785bb005fdc591e399ec24e0dfedf0b00b2
Stella-055 <stellaongayo243@gmail.com>
2025-06-23 15:46:16 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div>
+      
+      <button>Get random advice</button>
+     </div>
+    </>
+  )
+}
+
+export default App
